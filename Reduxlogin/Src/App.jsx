b64c11b8d9c5ae73928bb64559d2fbbd2985e2db
ShaistaDaniya <shaista@gigandtake.com>
2023-07-05 06:52:42 +0530

    #15

--- Reduxlogin/Src/App.jsx
+++ Reduxlogin/Src/App.jsx
@@ -8,7 +8,7 @@ import Screen2 from './Screen2';
 import Screen3 from './Screen3';
 import Screen4 from './Screen4';
 import Screen5 from './Screen5';
-import { useApi } from './api';
+import { useApi } from './Api';
 
 const Stack = createNativeStackNavigator();
 
